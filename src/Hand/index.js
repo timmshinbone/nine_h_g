0408b9005992df9e5e393440f86466ee9a10173c
Timm Schoenborn <timmschoenborn@gmail.com>
2020-05-10 19:03:27 -0500

    successfully converted hand to use hooks

--- src/Hand/index.js
+++ src/Hand/index.js
@@ -1,82 +1,70 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './index.css'
 import Card from '../Card'
 
+export default function Hand(props){
+	const [hand, setHand] = useState(props.deck)
+	const [players, setPlayers] = useState({name: 'timm', score: 0})
+	let [showingCount, setShowingCount] = useState(0)
+	const [rowPick, setRowPick] = useState(true)
+	const [refresh, setRefresh] = useState(true)
 
-class Hand extends Component {
-	constructor(props){
-		super(props)
-		this.state={
-			deck: this.props.deck,
-			players: [
-				{name: 'timm', score: 0}
-			],
-			cardsInDeck: [],
-			cardsInHands: [],
-			showingCount: 0,
-			rowPick: true,
-		}
-	}
-
-	placeCards(i){
-		const deck = this.state.deck
+	const placeCards = (i) => {
+		// const deck = this.state.deck
 		//deck is the player's hand deck from Game State(switch to Round state)
 		return(
 			<Card 
 				index = {i}
-				name={deck[i].name}
-				suit={deck[i].suit}
-				val={deck[i].val}
-				showing={deck[i].showing}
+				name={hand[i].name}
+				suit={hand[i].suit}
+				val={hand[i].val}
+				showing={hand[i].showing}
 				onClick={
-							this.props.drawnCard == null
+							props.drawnCard == null
 							?
-								() => this.rowFlip(deck[i])
+								() => rowFlip(hand[i])
 							:
-								() => this.swapCard(deck[i])
+								() => swapCard(hand[i])
 
 						}
 			/>
 		)
 	}
-
-	rowFlip(card){
-		const rowPick = this.state.rowPick
-		let showingCount = this.state.showingCount
-		const pHand = this.state.deck
-		if(this.state.rowPick === true){
+	const rowFlip = (card) => {
+		const pHand = hand
+		if(rowPick === true){
 			//refactor this so that it changes when a round activates rowpick
 			//rowpick is it's own three click function that gauges the placement
 			//of the cards.
 			if(showingCount < 2){
+				console.log(hand);
 				console.log(card);
 				pHand[pHand.indexOf(card)].showing = !card.showing
+				console.log(card);
 				console.log(showingCount);
-				this.setState({
-					deck: this.props.deck,
-					showingCount: this.state.showingCount += 1
-				})
+				setShowingCount(showingCount += 1)
+				setHand(pHand)
+				
 			} else {
+				console.log(hand);
 				console.log(card);
 				pHand[pHand.indexOf(card)].showing = !card.showing
+				console.log(hand);
+				showingCount += 1
 				console.log(showingCount);
-				this.setState({
-					deck: this.props.deck,
-					showingCount: this.state.showingCount += 1,
-					rowPick: false
-				})
+				setHand(pHand)
+				setRowPick(false)
 			}
 
 		} else {
 			console.log("It's not time yet!");
 		}
 	}
-
-	swapCard(card){
+	const swapCard = (card) => {
 		console.log('the card clicked');
 		console.log(card);
-		let drawn = this.props.drawnCard
-		let pHand = this.state.deck
+		let drawn = props.drawnCard
+		let pHand = hand
 		console.log('this is the whole hand');
 		console.log(pHand);
 		console.log('this is the card clicked');
@@ -88,35 +76,147 @@ class Hand extends Component {
 		
 		pHand.splice(pHand.indexOf(card), 1, drawn);
 		// console.log(pHand);
-		this.setState({
-			deck: pHand,
-		})
-		
+		setHand(pHand)
+		setRefresh(!refresh)
 	}
-
-	render(){
-
-		return (
-			<div className="player-hand">
-				<div className="board-row">
-					{this.placeCards(0)}
-					{this.placeCards(1)}
-					{this.placeCards(2)}
-				</div>
-				<div className="board-row">
-					{this.placeCards(3)}
-					{this.placeCards(4)}
-					{this.placeCards(5)}
-				</div>
-				<div className="board-row">
-					{this.placeCards(6)}
-					{this.placeCards(7)}
-					{this.placeCards(8)}
-				</div>
+	console.log('this is cards in hand', hand);
+	return (
+		<div className="player-hand">
+			<div className="board-row">
+				{placeCards(0)}
+				{placeCards(1)}
+				{placeCards(2)}
 			</div>
+			<div className="board-row">
+				{placeCards(3)}
+				{placeCards(4)}
+				{placeCards(5)}
+			</div>
+			<div className="board-row">
+				{placeCards(6)}
+				{placeCards(7)}
+				{placeCards(8)}
+			</div>
+		</div>
 
-		)
-	}
+	)
 }
 
-export default Hand
+
+// class Hand extends Component {
+// 	constructor(props){
+// 		super(props)
+// 		this.state={
+// 			deck: this.props.deck,
+// 			players: [
+// 				{name: 'timm', score: 0}
+// 			],
+// 			cardsInDeck: [],
+// 			cardsInHands: [],
+// 			showingCount: 0,
+// 			rowPick: true,
+// 		}
+// 	}
+
+// 	placeCards(i){
+// 		const deck = this.state.deck
+// 		//deck is the player's hand deck from Game State(switch to Round state)
+// 		return(
+// 			<Card 
+// 				index = {i}
+// 				name={deck[i].name}
+// 				suit={deck[i].suit}
+// 				val={deck[i].val}
+// 				showing={deck[i].showing}
+// 				onClick={
+// 							this.props.drawnCard == null
+// 							?
+// 								() => this.rowFlip(deck[i])
+// 							:
+// 								() => this.swapCard(deck[i])
+
+// 						}
+// 			/>
+// 		)
+// 	}
+
+// 	rowFlip(card){
+// 		const rowPick = this.state.rowPick
+// 		let showingCount = this.state.showingCount
+// 		const pHand = this.state.deck
+// 		if(this.state.rowPick === true){
+// 			//refactor this so that it changes when a round activates rowpick
+// 			//rowpick is it's own three click function that gauges the placement
+// 			//of the cards.
+// 			if(showingCount < 2){
+// 				console.log(card);
+// 				pHand[pHand.indexOf(card)].showing = !card.showing
+// 				console.log(showingCount);
+// 				this.setState({
+// 					deck: this.props.deck,
+// 					showingCount: this.state.showingCount += 1
+// 				})
+// 			} else {
+// 				console.log(card);
+// 				pHand[pHand.indexOf(card)].showing = !card.showing
+// 				console.log(showingCount);
+// 				this.setState({
+// 					deck: this.props.deck,
+// 					showingCount: this.state.showingCount += 1,
+// 					rowPick: false
+// 				})
+// 			}
+
+// 		} else {
+// 			console.log("It's not time yet!");
+// 		}
+// 	}
+
+// 	swapCard(card){
+// 		console.log('the card clicked');
+// 		console.log(card);
+// 		let drawn = this.props.drawnCard
+// 		let pHand = this.state.deck
+// 		console.log('this is the whole hand');
+// 		console.log(pHand);
+// 		console.log('this is the card clicked');
+// 		console.log(pHand[pHand.indexOf(card)]);
+// 		console.log('this is the card drawn');
+// 		console.log(drawn);
+// 		console.log('is the card showing');
+// 		console.log(card.showing);
+		
+// 		pHand.splice(pHand.indexOf(card), 1, drawn);
+// 		// console.log(pHand);
+// 		this.setState({
+// 			deck: pHand,
+// 		})
+		
+// 	}
+
+// 	render(){
+
+// 		return (
+// 			<div className="player-hand">
+// 				<div className="board-row">
+// 					{this.placeCards(0)}
+// 					{this.placeCards(1)}
+// 					{this.placeCards(2)}
+// 				</div>
+// 				<div className="board-row">
+// 					{this.placeCards(3)}
+// 					{this.placeCards(4)}
+// 					{this.placeCards(5)}
+// 				</div>
+// 				<div className="board-row">
+// 					{this.placeCards(6)}
+// 					{this.placeCards(7)}
+// 					{this.placeCards(8)}
+// 				</div>
+// 			</div>
+
+// 		)
+// 	}
+// }
+
+// export default Hand
